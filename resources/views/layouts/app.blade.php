5fcf867a08377dbd69114fe8744685a4c87c235e
phucnt <phucnt@runsystem.net>
2017-12-06 12:14:55 +0900

    Minor Changes to clean up blade files and straggling SASS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,60 +12,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
+  @include('_includes.nav.main')
+
   <div id="app">
-    <nav class="nav has-shadow" >
-      <div class="container">
-        <div class="nav-left">
-          <a class="nav-item" href="{{route('home')}}">
-            <img src="{{asset('images/phucnt-logo.png')}}" alt="Phucnt logo">
-          </a>
-          <a class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
-          <a class="nav-item is-tab is-hidden-mobile">Discuss</a>
-          <a class="nav-item is-tab is-hidden-mobile">Share</a>
-        </div>
-
-        <div class="nav-right" style="overflow: visible;">
-          @if (Auth::guest())
-            <a class="nav-item is-tab" href="{{route('login')}}">Login</a>
-            <a class="nav-item is-tab" href="{{route('register')}}">Join the Community</a>
-          @else
-            <button class="dropdown nav-item is-tab is-aligned-right">
-              Hey {{ Auth::user()->name }}
-              <span class="icon">
-                <i class="fa fa-caret-down"></i>
-              </span>
-
-              <ul class="dropdown-menu">
-                <li><a href="#">
-                  <span class="icon"><i class="fa fa-fw fa-user-circle-o m-r-10"></i></span>
-                  Profile
-                </a></li>
-                <li><a href="#">
-                  <span class="icon"><i class="fa fa-fw fa-bell m-r-10"></i></span>
-                  Notifications
-                </a></li>
-                <li><a href="#">
-                  <span class="icon"><i class="fa fa-fw fa-cog m-r-10"></i></span>
-                  Setting
-                </a></li>
-                <li class="seperator"></li>
-                <li><a href="{{route('logout')}}">
-                  <span class="icon"><i class="fa fa-fw fa-sign-out m-r-10"></i></span>
-                  Logout
-                </a></li>
-              </ul>
-            </button>
-          @endif
-        </div>
-      </div>
-    </nav>
-
     @yield('content')
   </div>
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}"></script>
+  @yield('scripts')
 </body>
 </html>
