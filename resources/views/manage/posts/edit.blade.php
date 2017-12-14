43e825d7ba413115dbd2ec5c1f8e1019f7f1a81e
PHUC Nguyen Trong <phucnt0@gmail.com>
2017-12-14 13:05:16 +0900

    Database Schema for our Blog Posts (Ep 20) - How to Build an Advanced Blog/CMS

---
+++ resources/views/manage/posts/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.manage')
+
+@section('content')
+  <div class="flex-container">
+    <div class="columns m-t-10">
+      <div class="column">
+        <h1 class="title">Edit Posts</h1>
+      </div>
+
+    </div>
+    <hr class="m-t-0">
+
+  </div>
+@endsection
+
+@section('scripts')
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {}
+    });
+  </script>
+@endsection
---
+++ resources/views/manage/posts/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.manage')
+
+@section('content')
+  <div class="flex-container">
+    <div class="columns m-t-10">
+      <div class="column">
+        <h1 class="title">Manage Posts</h1>
+      </div>
+      <div class="column">
+        <a href="{{route('posts.create')}}" class="button is-primary is-pulled-right">
+          <i class="fa fa-plus m-r-10"></i>
+          Create New Post
+        </a>
+      </div>
+    </div>
+    <hr class="m-t-0">
+
+  </div>
+@endsection
+
+@section('scripts')
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {}
+    });
+  </script>
+@endsection
